7a49098d2f22f707332cdc95d90b9c33db6eebcd
shes50103 <shes50103@gmail.com>
2020-05-29 23:23:00 +0800

    rails generate scaffold product name money:decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_151952) do
+ActiveRecord::Schema.define(version: 2020_05_29_152247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "money"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
